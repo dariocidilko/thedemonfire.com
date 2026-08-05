b649a2361ed7580611f5bc932dc2d77506d9e9f6
Dario Cidilko <dariocidilko@protonmail.com>
2026-08-05 23:53:21 +0200

    Added the data for upcoming releases and a fallback Spotify artist link (05/08/2026)

---
+++ data.js
@@ -0,0 +1,22 @@
+
+/* --------------------------------------------------------------------------
+
+   UPCOMING RELEASES
+
+   Date: Always write the date in year-month-day format.
+   Title: The name of the release. Use the same capitalization as on Spotify.
+   Feat: Write the name of the featured artist(s) on the release. (Optional)
+   Note: A short note about the release. (Optional)
+   URL: A pre-save or announcement link. (Optional)
+  
+   -------------------------------------------------------------------------- */
+
+const UPCOMING = [
+  {date: "2026-09-18", title: "Pride", feat: "", note: "", url: ""},
+  {date: "2026-10-09", title: "Get Paid", feat: "", note: "", url: ""},
+  {date: "2026-11-06", title: "You Don't Know Me", feat: "[REDACTED]", note: "", url: ""},
+  {date: "2026-12-25", title: "Autopilot", feat: "[REDACTED]", note: "", url: ""},
+];
+
+// This Spotify artist page is used as a fallback link for releases that don't have a URL. It is also the account the automatic updater reads the back catalogue from.
+const SPOTIFY_ARTIST = "https://open.spotify.com/artist/5WlSr320ygP7poNts98aXH";
